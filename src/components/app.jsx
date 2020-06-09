10fc415223f9878296e918f3e20ea23c5fccad2a
Arpan Kumar Pal <58396070+arpan13@users.noreply.github.com>
2020-06-09 20:06:54 +0530

    Added video and embedded youtube video in the website ,changes some css

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -51,71 +51,70 @@ function App(){
 
    
      return (
-         
-      <div className="container">
-         <div className="row ">
-           <div className="col-lg heading"><Header /></div>
-          
-         </div>
-         <div className="row">
-            <div className="dictionary"> 
-                {/* {coviddataIndia.map(creataCard)} */}
-                <Card 
-                imageflag={coviddataIndia.countryInfo.flag}
-                countryName={coviddataIndia.country}
-                confirmed={coviddataIndia.cases}
-                recoverd={coviddataIndia.recovered}
-                deaths={coviddataIndia.deaths}
-                />
-                <Card 
-                imageflag={coviddataUSA.countryInfo.flag}
-                countryName={coviddataUSA.country}
-                confirmed={coviddataUSA.cases}
-                recoverd={coviddataUSA.recovered}
-                deaths={coviddataUSA.deaths}
-                />
-                <Card 
-                imageflag={coviddataChina.countryInfo.flag}
-                countryName={coviddataChina.country}
-                confirmed={coviddataChina.cases}
-                recoverd={coviddataChina.recovered}
-                deaths={coviddataChina.deaths}
-                />
-               
-                
-            
-                </div>
-         </div>
-         <div class="row">
-               
-              <div class="flourish-embed flourish-chart  col-lg-6 " data-src="story/230114">
-             </div>
-             <div class="flourish-embed flourish-map  col-lg-6 " data-src="story/229998">
-             </div>
-            
+         <div>
+              <div className="container">
+                    <div className="row ">
+                      <div className="col-lg heading"><Header /></div>
+                    </div>
+                    <div className="row">
+                        <div className="dictionary"> 
+                            {/* {coviddataIndia.map(creataCard)} */}
+                            <Card 
+                            imageflag={coviddataIndia.countryInfo.flag}
+                            countryName={coviddataIndia.country}
+                            confirmed={coviddataIndia.cases}
+                            recoverd={coviddataIndia.recovered}
+                            deaths={coviddataIndia.deaths}
+                            />
+                            <Card 
+                            imageflag={coviddataUSA.countryInfo.flag}
+                            countryName={coviddataUSA.country}
+                            confirmed={coviddataUSA.cases}
+                            recoverd={coviddataUSA.recovered}
+                            deaths={coviddataUSA.deaths}
+                            />
+                            <Card 
+                            imageflag={coviddataChina.countryInfo.flag}
+                            countryName={coviddataChina.country}
+                            confirmed={coviddataChina.cases}
+                            recoverd={coviddataChina.recovered}
+                            deaths={coviddataChina.deaths}
+                            />
                         
-         </div>
-
-         
-           
-                {/* <div className="dictionaryGraph">
-                    <Graph />
+                            
+                        
+                            </div>
+                    </div>
+                    <div class="row">
+                        
+                        <div class="flourish-embed flourish-chart  col-lg-6 " data-src="story/230114">
+                        </div>
+                        <div class="flourish-embed flourish-map  col-lg-6 " data-src="story/229998">
+                        </div>
+                        
+                                    
+                    </div>
                 </div>
-                <div className="dictionaryGraph">
-                    <GraphMap /> 
-                </div> */}
+                <div className="container-fluid">
+                   <h1 className="videoh1">Some videos to watch</h1>
+                   <div className="row video ">
+                        <div className="col-md-6">
+                        <iframe width="460" height="315" src="https://www.youtube.com/embed/W-zhhSQDD1U" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                        <div className="col-md-6">
+                        <iframe width="460" height="315" src="https://www.youtube.com/embed/TjcoN9Aek24" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                        
+
+                   </div>
+
+                </div>
                 
-            
-            
-          <div className="row">
-             <div className="col-lg foot">
-                <Footer />
-             </div>
-             
-          </div>  
-          
-          
-      </div>
+     
+         </div>
+         
+      
+    
       
           
   )
